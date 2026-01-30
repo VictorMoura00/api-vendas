00559052d8898fcf6873e8dfd2e89fcb904159d7
VictorMoura00 <victormoura@proton.me>
2026-01-29 23:33:03 -0300

    feat(errors): adicionar classe NotFoundError para tratamento de erros de recurso não encontrado

---
+++ src/common/domain/error/not-found-error.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { AppError } from './app-error'
+
+export class NotFoundError extends AppError {
+  constructor(message: string = 'Resource not found') {
+    super(message, 404)
+    this.name = 'NotFoundError'
+  }
+}
