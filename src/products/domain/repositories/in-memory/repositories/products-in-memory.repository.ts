47640c20a9c5b15b3d87801cb4300e1f5fccb23a
VictorMoura00 <victormoura@proton.me>
2026-01-30 23:24:53 -0300

    feat(product): criado repository da entidade products

---
+++ src/products/domain/repositories/in-memory/repositories/products-in-memory.repository.ts
@@ -0,0 +1,56 @@
+import { InMemoryRepository } from '@/common/domain/repositories/in-memory.repository'
+import { ProductModel } from '@/products/domain/models/poducts.model'
+import { ProductId, ProductsRepository } from '../../products.repository'
+import { NotFoundError } from '@/common/domain/error/not-found-error'
+import { ConflictError } from '@/common/domain/error/conflict-error'
+
+export class ProductsInMemoryRepository
+  extends InMemoryRepository<ProductModel>
+  implements ProductsRepository
+{
+  sortableFields: string[] = ['name', 'updated_at']
+
+  async findByName(name: string): Promise<ProductModel> {
+    const product = this.items.find(item => item.name === name)
+    if (!product) {
+      throw new NotFoundError(`Product not found using name ${name}`)
+    }
+    return product
+  }
+
+  async findAllByIds(productIds: ProductId[]): Promise<ProductModel[]> {
+    const existingProducts = []
+    for (const productId of productIds) {
+      const product = this.items.find(item => item.id === productId.id)
+      if (product) {
+        existingProducts.push(product)
+      }
+    }
+    return existingProducts
+  }
+
+  async conflictingName(name: string): Promise<void> {
+    const product = this.items.find(item => item.name === name)
+    if (product) {
+      throw new ConflictError(`Name already used on another product`)
+    }
+  }
+
+  protected async applyFilter(
+    items: ProductModel[],
+    filter: string | null,
+  ): Promise<ProductModel[]> {
+    if (!filter) return items
+    return items.filter(item =>
+      item.name.toLowerCase().includes(filter.toLowerCase()),
+    )
+  }
+
+  protected async applySort(
+    items: ProductModel[],
+    sort: string | null,
+    sort_dir: string | null,
+  ): Promise<ProductModel[]> {
+    return super.applySort(items, sort ?? 'created_at', sort_dir ?? 'desc')
+  }
+}
