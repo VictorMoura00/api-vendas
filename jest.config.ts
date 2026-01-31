4abf8605bcefb43b791425914a77cd75da5d3a69
VictorMoura00 <victormoura@proton.me>
2026-01-30 22:40:10 -0300

    feat(test): configurado Jest para rodar os testes

--- jest.config.ts
+++ jest.config.ts
@@ -1,16 +1,26 @@
 import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from './tsconfig.json'
+import { readFileSync } from 'fs'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const tsConfig = JSON.parse(
+  readFileSync(resolve(__dirname, './tsconfig.json'), 'utf-8'),
+)
+const compilerOptions = tsConfig.compilerOptions || {}
 
 export default {
-moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-        prefix: '<rootDir>/',
-    }),
-    testRegex: '.*\\.spec\\.ts$',
-    transform: {
-        '^.+\\.(t|j)s$': 'ts-jest',
-    },
-    collectConverageFrom: ['**/*.(t|j)s'],
-    coverageDirectory: '../coverage',
-    testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
 }
