58e7818df164817c089ca9fe0b9b3b8644c33b4c
VictorMoura00 <victormoura@proton.me>
2026-01-30 22:41:11 -0300

    test(iRepository): testes da interface da Repository abstrata

--- src/common/domain/repositories/in-memory.repository.ts
+++ src/common/domain/repositories/in-memory.repository.ts
@@ -3,12 +3,12 @@ import { randomUUID } from 'node:crypto'
 import { NotFoundError } from '../error/not-found-error'
 import {
   RepositoryInterface,
-  SearchOutput,
   SearchInput,
+  SearchOutput,
 } from './repository.interface'
 
 export type ModelProps = {
-  id: string
+  id?: string
   [key: string]: any
 }
 
@@ -16,75 +16,69 @@ export type CreateProps = {
   [key: string]: any
 }
 
-export abstract class InMemoryRepository<Model extends ModelProps, CreateProps>
+export abstract class InMemoryRepository<Model extends ModelProps>
   implements RepositoryInterface<Model, CreateProps>
 {
+  findAll(): Promise<Model[]> {
+    throw new Error('Method not implemented.')
+  }
   items: Model[] = []
   sortableFields: string[] = []
 
-  create(_props: CreateProps): Model {
+  create(props: CreateProps): Model {
     const model = {
       id: randomUUID(),
       created_at: new Date(),
       updated_at: new Date(),
-      ..._props,
+      ...props,
     }
     return model as unknown as Model
   }
 
-  async insert(_model: Model): Promise<Model> {
-    this.items.push(_model)
-    return _model
+  async insert(model: Model): Promise<Model> {
+    this.items.push(model)
+    return model
   }
 
-  findAll(): Promise<Model[]> {
-    throw new Error('Method not implemented.')
+  async findById(id: string): Promise<Model> {
+    return this._get(id)
   }
 
-  async findById(_id: string): Promise<Model | null> {
-    return this._get(_id)
+  async update(model: Model): Promise<Model> {
+    await this._get(model.id)
+    const index = this.items.findIndex(item => item.id === model.id)
+    this.items[index] = model
+    return model
   }
 
-  async update(_model: Model): Promise<Model> {
-    await this._get(_model.id)
-    const index = this.items.findIndex(item => item.id === _model.id)
-    this.items[index] = _model
-    return _model
-  }
-
-  async delete(_id: string): Promise<void> {
-    await this._get(_id)
-    const index = this.items.findIndex(item => item.id === _id)
+  async delete(id: string): Promise<void> {
+    await this._get(id)
+    const index = this.items.findIndex(item => item.id === id)
     this.items.splice(index, 1)
   }
 
-  async search(_props: SearchInput): Promise<SearchOutput<Model>> {
-    const page = _props.page ?? 1
-    const per_page = _props.per_page ?? 15
-    const sort = _props.sort ?? null
-    const sort_dir = _props.sort_dir ?? null
-    const filter = _props.filter ?? null
+  async search(props: SearchInput): Promise<SearchOutput<Model>> {
+    const page = props.page ?? 1
+    const per_page = props.per_page ?? 15
+    const sort = props.sort ?? null
+    const sort_dir = props.sort_dir ?? null
+    const filter = props.filter ?? null
 
     const filteredItems = await this.applyFilter(this.items, filter)
-    const sorted_items = await this.applySort(
-      filteredItems,
-      sort,
-      sort_dir,
-    )
+    const orderedItems = await this.applySort(filteredItems, sort, sort_dir)
     const paginatedItems = await this.applyPaginate(
-      sorted_items,
+      orderedItems,
       page,
       per_page,
     )
-
     return {
       items: paginatedItems,
-      per_page: per_page,
       total: filteredItems.length,
       current_page: page,
-      sort: sort,
-      sort_dir: sort_dir,
-      filter: filter,
+      per_page,
+      sort,
+      sort_dir,
+      filter,
       last_page: Math.ceil(filteredItems.length / per_page),
     }
   }
@@ -104,12 +98,10 @@ export abstract class InMemoryRepository<Model extends ModelProps, CreateProps>
     }
 
     return [...items].sort((a, b) => {
-      const a_value = a[sort]
-      const b_value = b[sort]
-      if (a_value < b_value) {
+      if (a[sort] < b[sort]) {
         return sort_dir === 'asc' ? -1 : 1
       }
-      if (a_value > b_value) {
+      if (a[sort] > b[sort]) {
         return sort_dir === 'asc' ? 1 : -1
       }
       return 0
@@ -122,14 +114,14 @@ export abstract class InMemoryRepository<Model extends ModelProps, CreateProps>
     per_page: number,
   ): Promise<Model[]> {
     const start = (page - 1) * per_page
-    const end = start + per_page
-    return items.slice(start, end)
+    const limit = start + per_page
+    return items.slice(start, limit)
   }
 
   protected async _get(id: string): Promise<Model> {
     const model = this.items.find(item => item.id === id)
     if (!model) {
-      throw new NotFoundError(`Model with id ${id} not found`)
+      throw new NotFoundError(`Model not found using ID ${id}`)
     }
     return model
   }
