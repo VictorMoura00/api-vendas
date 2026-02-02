dc64b46ca665c313bd167c99a4fa9a226b155a81
VictorMoura00 <victormoura@proton.me>
2026-02-02 16:29:22 -0300

    fix(jest): ajuste de configuração do jest init

--- jest.int.config.ts
+++ jest.int.config.ts
@@ -1,5 +1,10 @@
 import { pathsToModuleNameMapper } from 'ts-jest'
-import { compilerOptions } from './tsconfig.json'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const tsconfigPath = path.join(process.cwd(), 'tsconfig.json')
+const tsconfig = JSON.parse(fs.readFileSync(tsconfigPath, 'utf-8'))
+const compilerOptions = tsconfig.compilerOptions
 
 export default {
   moduleFileExtensions: ['ts', 'js', 'json'],
@@ -10,6 +15,10 @@ export default {
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
+  // ADIÇÃO IMPORTANTE:
+  // Diz ao Jest para transformar arquivos JS, mas NÃO ignorar o faker.
+  // A regex significa: "Ignore node_modules, a menos que seja @faker-js/faker"
+  transformIgnorePatterns: ['node_modules/(?!@faker-js/faker)'],
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
